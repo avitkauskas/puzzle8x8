daebd42904f3c56c91c2e70f171d8a14a0f5622f
Alvydas Vitkauskas <alvydas.vitkauskas@gmail.com>
2019-03-03 16:58:32 +0200

    use srtuct equality in specs

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # puzzle8x8
 
 This is a play project in Crystal lang to analyse the 8x8 field puzzle
-
-## Contributors
-
-- [Alvydas Vitkauskas](https://github.com/avitkauskas) - creator and maintainer
--- spec/field_spec.cr
+++ spec/field_spec.cr
@@ -1,154 +1,154 @@
 require "./spec_helper"
-require "../src/utils"
+require "../src/field"
 
 describe Field do
-  describe "#shift_right" do
+  describe ".shift_right" do
     it "shifts the field one column to the right" do
       original = Factory.original
       shifted_right = Factory.shifted_right
-      original.shift_right.field.should eq shifted_right.field
+      original.shift_right.should eq shifted_right
     end
 
     it "should not modify the original" do
       original = Factory.original
       copy = Factory.original
       original.shift_right
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#shift_down" do
+  describe ".shift_down" do
     it "shifts the field one column down" do
       original = Factory.original
       shifted_down = Factory.shifted_down
-      original.shift_down.field.should eq shifted_down.field
+      original.shift_down.should eq shifted_down
     end
 
     it "should not modify the original" do
       original = Factory.original
       copy = Factory.original
       original.shift_down
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#flush_left" do
+  describe ".flush_left" do
     it "flushes the field all the way to the left" do
       original = Factory.original
       flushed_left = Factory.flushed_left
-      original.flush_left.field.should eq flushed_left.field
+      original.flush_left.should eq flushed_left
     end
 
     it "should not modify the original" do
       original = Factory.original
       copy = Factory.original
       original.flush_left
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#flush_top" do
+  describe ".flush_top" do
     it "flushes the field all the way to the top" do
       original = Factory.original
       flushed_top = Factory.flushed_top
-      original.flush_top.field.should eq flushed_top.field
+      original.flush_top.should eq flushed_top
     end
 
     it "should not modify the original" do
       original = Factory.original
       copy = Factory.original
       original.flush_top
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#flip_diagonal" do
+  describe ".flip_diagonal" do
     it "flips the field by diagonal" do
       original = Factory.original
       flipped_diagonal = Factory.flipped_diagonal
-      original.flip_diagonal.field.should eq flipped_diagonal.field
+      original.flip_diagonal.should eq flipped_diagonal
     end
 
     it "should not modify the original" do
       original = Factory.original
       copy = Factory.original
       original.flip_diagonal
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#flip_horizontal" do
+  describe ".flip_horizontal" do
     it "flips the field horizontaly" do
       original = Factory.original
       flipped_horizontal = Factory.flipped_horizontal
-      original.flip_horizontal.field.should eq flipped_horizontal.field
+      original.flip_horizontal.should eq flipped_horizontal
     end
 
     it "should not modify the original" do
       original = Factory.original
       copy = Factory.original
       original.flip_horizontal
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#rotate" do
+  describe ".rotate" do
     it "rotates field 90 degrees clockwise" do
       original = Factory.original
       rotated = Factory.rotated
-      original.rotate.field.should eq rotated.field
+      original.rotate.should eq rotated
     end
 
     it "should not modify the original" do
       original = Factory.original
       copy = Factory.original
       original.rotate
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#flip_and_flush" do
+  describe ".flip_and_flush" do
     it "flips field and flushes it top-left" do
       original = Factory.original
       flipped_flushed = Factory.flipped_flushed
-      original.flip_and_flush.field.should eq flipped_flushed.field
+      original.flip_and_flush.should eq flipped_flushed
     end
 
     it "should not modify the original" do
       original = Factory.original
       copy = Factory.original
       original.flip_and_flush
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#rotate_and_flush" do
+  describe ".rotate_and_flush" do
     it "rotates field and flushes it top-left" do
       original = Factory.original
       rotated_flushed = Factory.rotated_flushed
-      original.rotate_and_flush.field.should eq rotated_flushed.field
+      original.rotate_and_flush.should eq rotated_flushed
     end
 
     it "should not modify the original" do
       original = Factory.original
       copy = Factory.original
       original.rotate_and_flush
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#put" do
+  describe ".put" do
     it "puts one field on the other" do
       original = Factory.original
       other = Factory.other
       combined = Factory.combined
-      original.put(other).field.should eq combined.field
+      original.put(other).should eq combined
     end
 
     it "should not put if it does not fit" do
       original = Factory.original
       other = Factory.shifted_right
-      original.put(other).field.should eq original.field
+      original.put(other).should eq original
     end
 
     it "should not modify the original" do
@@ -156,22 +156,22 @@ describe Field do
       other = Factory.other
       copy = Factory.original
       original.put other
-      original.field.should eq copy.field
+      original.should eq copy
     end
   end
 
-  describe "#remove" do
+  describe ".remove" do
     it "removes one field from the other" do
       original = Factory.original
       other = Factory.other
       combined = Factory.combined
-      combined.remove(other).field.should eq original.field
+      combined.remove(other).should eq original
     end
 
     it "should not do anything if field to remove was not put" do
       original = Factory.original
       other = Factory.shifted_right
-      original.remove(other).field.should eq original.field
+      original.remove(other).should eq original
     end
 
     it "should not modify the original" do
@@ -179,7 +179,7 @@ describe Field do
       other = Factory.other
       copy = Factory.combined
       combined.remove other
-      combined.field.should eq copy.field
+      combined.should eq copy
     end
   end
 end
