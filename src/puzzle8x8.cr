1fa1429ad590b179b0824aff600f1a6bbc6f5237
Alvydas Vitkauskas <alvydas.vitkauskas@gmail.com>
2019-03-03 19:00:27 +0200

    describe board and pieces

--- src/puzzle8x8.cr
+++ src/puzzle8x8.cr
@@ -1,26 +1,16 @@
-require "./utils"
+require "./field"
+require "./board"
+require "./pieces"
 
-empty = Field.new
+def get_piece_positions(piece)
+  positions = Set.new
+  pos = piece.flush_left.flush_top
+  positions << pos
+  pos = pos.flip_and_flush
+  positions << pos
 
-f1 = Field.new 0b_11110000_11100000_10000000_00000000_00000000_00000000_00000000_00000000
+end
 
-f2 = Field.new(<<-FIELD)
-     11110011
-     11100000
-     10000000
-     FIELD
-
-# empty.put f1
-empty.put f2
-empty.remove f1.shift_right.shift_right.shift_right
-
-puts empty, f1
-
-# puts f1
-# puts f1.rotate
-# puts f1
-# puts f1.flip_and_flush
-# puts f1.rotate_and_flush
-# puts f1.rotate
-#
-# puts f1.field
+PIECES.each do |piece|
+  puts piece
+end
